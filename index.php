f2675ae59d7f6631008f37fae3638a4fb8ecb2a1
Arif Budiman <arif.budiman41@ui.ac.id>
2018-01-01 18:48:59 +0700

    init website

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Jankric Respawn</title>
+	<link rel="shortcut icon" href="img/jankric-logo.png" type="image/x-icon">
+	<link rel="icon" href="img/jankric-logo.png" type="image/x-icon">
+	
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/grayscale.min.css" rel="stylesheet">
+	<link href="css/mycss.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+		<img src="img/jrt.png" class="navbar-brand js-scroll-trigger" href="#page-top" alt="Jankric Respawn" style="width:80px;height:40px;">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#work">Project</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="masthead">
+      <div class="intro-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <h1 class="brand-heading">Jankric Respawn</h1>
+              <p class="intro-text">we do great stuffs ></p>
+              <a href="#work" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+	
+	<!-- Content Section -->
+	<section id="content" class="content-section text-center">
+		<div class="container">
+		<h5 id="work">Our Current Works :</h5>
+			<div class="row">
+				<div class="card bg-black mx-3" style="width: 28.5%;" >
+				  <img class="card-img-top" src="img/ProjectLilyThumb1k.png" alt="Card image cap">
+				  <div class="card-body">
+					<a href="#" class="btn btn-card ">I AM NOT INNOCENT</a>
+					<br>
+					<p class="card-text text-justify">A Third person action game that driven by a story. Player will be stepping in the shoes of Lily. Our Main Character.</p>
+				  </div>
+				</div>
+				<div class="card bg-black mx-3" style="width: 28.5%;" >
+				  <img class="card-img-top" src="img/Iwwu1k.png" alt="Card image cap">
+				  <div class="card-body">
+					<a href="#" class="btn btn-card">I WANNA WAKE UP</a>
+					<br>
+					<p class="card-text text-justify">A first person puzzle game that makes you think and dig the enviroment about what's really happened.</p>
+				  </div>
+				</div>
+				<div class="card bg-black mx-3" style="width: 28.5%;" >
+				  <img class="card-img-top" src="img/ThePlant1k.jpg" alt="Card image cap">
+				  <div class="card-body">
+					<a href="#" class="btn btn-card">THE PLANT</a>
+					<br>
+					<p class="card-text text-justify">A Short Movie set In 2070, Where Rare Plants Experts found a huge big tree that has a large amount of renewable energy</p>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <!-- About Section -->
+    <section id="about" class="team-section content-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2>About</h2>
+			<img class="img-fluid" src="img/LogoJankric1x05.png" alt="Logo" style="width: 35%;">
+			<br><br>
+            <p>Jankric Respawn is an Indie Game Developer from Depok, Jawa Barat. Indonesia.</p>
+            <p>Formed by a group of teens whom bet their time learning to make great games.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Our Team Section -->
+    <!--section id="our-team" class="team-section content-section text-center">
+      <div class="container">
+        <div class="col-lg-8 mx-auto">
+          <h2>Our Team</h2>
+          <p>The best and the brightest</p>
+          <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+        </div>
+      </div>
+    </section-->
+
+    <!-- Contact Section -->
+    <section id="contact" class="content-section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h2>Contact Start Bootstrap</h2>
+            <p>Feel free to leave us a comment on the
+              <a href="http://startbootstrap.com/template-overviews/grayscale/">Grayscale template overview page</a>
+              on Start Bootstrap to give some feedback about this theme!</p>
+            <ul class="list-inline banner-social-buttons">
+              <li class="list-inline-item">
+                <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg">
+                  <i class="fa fa-twitter fa-fw"></i>
+                  <span class="network-name">Twitter</span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://github.com/BlackrockDigital/startbootstrap" class="btn btn-default btn-lg">
+                  <i class="fa fa-github fa-fw"></i>
+                  <span class="network-name">Github</span>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg">
+                  <i class="fa fa-google-plus fa-fw"></i>
+                  <span class="network-name">Google+</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+	
+    <!-- Footer -->
+    <footer>
+      <div class="container text-center">
+        <p>Copyright &copy; Jankric Respawn 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <!--script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script-->
+
+    <!-- Custom scripts for this template -->
+    <script src="js/grayscale.min.js"></script>
+
+  </body>
+
+</html>
