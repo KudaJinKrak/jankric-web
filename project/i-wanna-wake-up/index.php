81091e04c0a5e8033f5feb816c64a01a60183971
Arif Budiman <arif.budiman41@ui.ac.id>
2018-03-25 13:14:50 +0700

    Add IWWU page

---
+++ project/i-wanna-wake-up/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>I Wanna Wake Up - Jankric Respawn</title>
+	<link rel="shortcut icon" href="../../img/jankric-logo.png" type="image/x-icon">
+	<link rel="icon" href="../../img/jankric-logo.png" type="image/x-icon">
+	
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../css/grayscale.min.css" rel="stylesheet">
+	<link href="../../css/mycss.css" rel="stylesheet">
+	<link href="../../css/blueimp-gallery.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+		<a class="js-scroll-trigger" href="../../">
+			<img src="../../img/jrt.png" class="navbar-brand js-scroll-trigger" href="#page-top" alt="Jankric Respawn" style="width:80px;height:40px;">
+		</a>
+		<a class="js-scroll-trigger" href="#jankric-head">
+			I Wanna Wake Up
+		</a>
+		<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+		    <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#work">Overview</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="../../#work">Project</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="../../#about">About</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="../../#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Intro Header -->
+    <header class="jankric-cover" id="jankric-head">
+      <div class="intro-body">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <!--h1 class="brand-heading">I Am Not Innocent</h1-->
+              <img src="../../img/Iwwu1k.png" class="navbar-brand js-scroll-trigger" href="#page-top" alt="Jankric Respawn" style="width: 40%;">
+			  <p class="intro-text">
+				<q>I Wanna Wake Up</q> <br>
+				<cite>codename : #iwwu</cite><br>
+				<a href="https://youtu.be/khNXf8QPkXQ" target="_blank" class="btn btn-trailer">WATCH Trailer</a>
+			  </p>
+              <a href="#work" class="btn btn-circle js-scroll-trigger">
+                <i class="fa fa-angle-double-down animated"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+	
+	<!-- Content Section -->
+	<section id="content" class="content-section">
+		<div id="work" class="container mx-auto" style="width: 75%;" >
+            <h2 class="text-center">[ Trailer ]</h2>
+			<div class="text-center"><iframe width="854" height="480" src="https://www.youtube.com/embed/khNXf8QPkXQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div><br>
+            
+            <h2 class="text-center">[ I Wanna Wake Up ]</h2>
+            <p>A first person puzzle game that makes you think and dig the enviroment about what's really happened.</p>
+            
+            <div class ="text-center">
+            <div id="demo" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+                <li data-target="#demo" data-slide-to="3"></li>
+                <li data-target="#demo" data-slide-to="4"></li>
+                <li data-target="#demo" data-slide-to="5"></li>
+                <li data-target="#demo" data-slide-to="6"></li>
+                <li data-target="#demo" data-slide-to="7"></li>
+                <li data-target="#demo" data-slide-to="8"></li>
+                <li data-target="#demo" data-slide-to="9"></li>
+                <li data-target="#demo" data-slide-to="10"></li>
+                <li data-target="#demo" data-slide-to="11"></li>
+                <li data-target="#demo" data-slide-to="12"></li>
+                </ul>
+
+                <!-- The slideshow -->
+                <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../../img/i-wanna-wake-up/9.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/2.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/3.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/10.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/5.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/6.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/7.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/8.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/1.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/4.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/12.png" width=66%;  alt="IWWU">
+                </div>
+                <div class="carousel-item">
+                    <img src="../../img/i-wanna-wake-up/13.png" width=66%;  alt="IWWU">
+                </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+                </a>
+
+            </div></div>
+			<p>Here, you just had an accident, your body gone coma, and stuck in the subconscious world. you must find the way out trough the puzzles made from memories and problems as you sorting it out and get back, get out, and wake up from this unconscious world.</p>
+			<h3 class="text-center"><a href="https://gamejolt.com/games/i-wanna-wake-up/123196">[Download]</a></h3>
+            <br>
+            <p class="text-center">Our developer-narrated gameplay</p>
+            <div class="text-center"><iframe width="854" height="480" src="https://www.youtube.com/embed/I6aZ5V8QCiU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+		</div>
+	</section>
+  
+    <!-- Footer -->
+    <footer>
+      <div class="container text-center">
+        <p>Copyright &copy; Jankric Respawn 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+
+    <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+    <!--script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script-->
+
+    <!-- Custom scripts for this template -->
+    <script src="../../js/grayscale.min.js"></script>
+	<script src="../../js/blueimp-gallery.min.js"></script>
+
+  </body>
+
+</html>
